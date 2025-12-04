8cb19d42ab26c05781c738ded6f6b0187b7da70d
HugoPerezArevalo <miguelyhugo57@gmail.com>
2025-12-04 17:13:51 +0100

    clases_terminadas

--- HundirFlota.py
+++ HundirFlota.py
@@ -1,12 +1,30 @@
 print("***Hundir la Flota***")
 
 class Barco:
-    pass
+    def __init__(self, longitud):
+        self.longitud = longitud
+        self.tocados = 0
+        
+    def esta_hundido(self):
+        #Si la cantidad de veces tocado es = a la longitud...
+        if self.tocados == self.longitud:
+            return True
+        else:
+            return False
 
 class Tablero:
-    pass
+    def __init__(self):
+        self.celdas = [] #Lista vacia
+        
+        #Creamos 10 filas
+        for i in range (10):
+            #Cada vuelta se añade una fila d agua
+            self.celdas.append(['~'] * 10)
 
 class Jugador:
-    pass
+    def __init__(self):
+        self.tablero_propio = Tablero()
+        self.tablero_rival = Tablero()
+        
 
 
